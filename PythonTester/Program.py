ca215279270052862931bcf28922b0c8530dfcde
kirahsosha <kirahsosha@gmail.com>
2025-12-08 12:17:32 +0800

    Add python tester

---
+++ PythonTester/Program.py
@@ -0,0 +1,7 @@
+import Solution
+
+
+def Test_198(self=None):
+    nums = [2,7,9,3,1]
+    res = Solution.rob(self, nums)
+    print(f', {res}')
---
+++ PythonTester/main.py
@@ -0,0 +1,9 @@
+from Program import Test_198
+
+
+def runTest():
+    Test_198()
+
+# 按装订区域中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+    runTest()
