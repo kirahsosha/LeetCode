2fe58019f7039a10adb1a6cbe3179fc961787171
kirahsosha <kirahsosha@gmail.com>
2026-06-17 03:36:49 +0800

    20260617

---
+++ Answer/3614.用特殊操作处理字符串-ii.go
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=3614 lang=golang
+ *
+ * [3614] 用特殊操作处理字符串 II
+ */
+
+// @lc code=start
+func processStr(s string, k int64) byte {
+	n := len(s)
+	size := make([]int64, n)
+	sz := int64(0)
+	for i, c := range s {
+		if c == '*' {
+			sz = max(sz-1, 0)
+		} else if c == '#' {
+			sz *= 2
+		} else if c != '%' { // c 是字母
+			sz++
+		}
+		size[i] = sz
+	}
+
+	if k >= size[n-1] { // 下标越界
+		return '.'
+	}
+
+	// 迭代
+	for i := n - 1; ; i-- {
+		c := s[i]
+		sz = size[i]
+		if c == '#' {
+			if k >= sz/2 { // k 在复制后的右半边
+				k -= sz / 2
+			}
+		} else if c == '%' {
+			k = sz - 1 - k // 反转前的下标为 sz-1-k 的字母就是答案
+		} else if c != '*' && k == sz-1 { // 找到答案
+			return c
+		}
+	}
+}
+
+// @lc code=end
